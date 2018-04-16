fa319fe437b20705b202541ede8e298e9b4ee3fa
AnaTravina <snowbreaker007@gmail.com>
2018-04-16 10:22:01 -0500

    h

--- Hw2Code2.R
+++
@@ -1,3 +0,0 @@
-unction named ‘fourbarpos’ that takes the following as inputs: 
-four link lengths ( ̄l), θ1, θ2, and δ, and generates the four angles θ ̄ as output.
- Note that δ = ±1 represents either open or cross chain.
